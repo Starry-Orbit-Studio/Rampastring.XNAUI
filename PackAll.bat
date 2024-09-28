a1aa441490b317f29d234b32217107397877de43
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-09-28 17:22:09 +0800

    生成符号包
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- PackAll.bat
+++ PackAll.bat
@@ -1,9 +1,9 @@
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXRelease
-dotnet pack .\Rampastring.XNAUI.csproj -c UniversalGLRelease
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLRelease
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsXNARelease
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c WindowsDXRelease
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c UniversalGLRelease
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c WindowsGLRelease
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c WindowsXNARelease
 
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXDebug
-dotnet pack .\Rampastring.XNAUI.csproj -c UniversalGLDebug
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLDebug
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsXNADebug
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c WindowsDXDebug
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c UniversalGLDebug
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c WindowsGLDebug
+dotnet pack .\Rampastring.XNAUI.csproj --include-symbols -c WindowsXNADebug
